6cc984fda9f2b3be7b8b957f4801fbe2ba41d663
chipres <ana.chipres16@tectijuana.edu.mx>
2016-11-18 14:50:48 -0800

    Add files via upload

---
+++ parser_test.go
@@ -0,0 +1,48 @@
+package chipres_test
+
+import (
+	"github.com/chipres/chipres_sintactico"
+	"io/ioutil"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+
+func TestParser_ParseStatement(t *testing.T) {
+	f, _ := ioutil.ReadFile("C://proyects//src//github.com//pasillasangel//pasillas_parser//Gramatica.txt")//modificar
+	z := string(f[:])
+	
+	var tests = []struct {
+		s    string
+		stmt *chipres.Statement
+		err  string
+	}{
+		
+		{
+			s:    z,
+			stmt: &chipres.Statement{
+	
+			},
+		},
+
+		
+	}
+
+	for i, tt := range tests {
+		stmt, err := chipres.NewParser(strings.NewReader(tt.s)).Parse()
+		if !reflect.DeepEqual(tt.err, errstring(err)) {
+			t.Errorf("%d. %q: error mismatch:\n  exp=%s\n  got=%s\n\n", i, tt.s, tt.err, err)
+		} else if tt.err == "" && !reflect.DeepEqual(tt.stmt, stmt) {
+			t.Errorf("%d. %q\n\nstmt mismatch:\n\nexp=%#v\n\ngot=%#v\n\n", i, tt.s, tt.stmt, stmt)
+		}
+	}
+}
+
+
+func errstring(err error) string {
+	if err != nil {
+		return err.Error()
+	}
+	return ""
+}
