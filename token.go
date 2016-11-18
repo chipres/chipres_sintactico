37ac916e74a26a25a138d0fb25d041923546f950
chipres <ana.chipres16@tectijuana.edu.mx>
2016-11-18 14:33:27 -0800

    Add files via upload

---
+++ token.go
@@ -0,0 +1,33 @@
+package pasillas
+
+// Token represents a lexical token.
+type Token int
+
+const (
+	// Special tokens
+	ILLEGAL Token = iota
+	EOF
+	WS
+
+	// Literals
+	IDENT // main
+
+	// Misc characters
+	ASTERISK // *
+	//COMMA    // ,
+	PUNTOYCOMA
+	DPUNTOS
+	PARDERE
+	PARIZQI
+	CORCHETIZQUI
+	CORCHETDERE
+
+	//Pregunta
+	cicloif
+	celseif
+	cicloelse
+
+	// Keywords
+	//SELECT
+	//FROM
+)
